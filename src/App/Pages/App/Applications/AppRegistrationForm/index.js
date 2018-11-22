36c7a6b1dc7bbdf1a9677e016b0e5e8229f01d3e
Joaquín Gumucio <jjgumucio@gmail.com>
2018-11-22 11:47:13 -0300

    Completing registration form

--- src/App/Pages/App/Applications/AppRegistrationForm/index.js
+++ src/App/Pages/App/Applications/AppRegistrationForm/index.js
@@ -3,16 +3,13 @@ import Section from 'App/components/Section'
 import Button from 'orionsoft-parts/lib/components/Button'
 import { Form, Field } from 'simple-react-form'
 import Text from 'orionsoft-parts/lib/components/fields/Text'
+import Select from 'orionsoft-parts/lib/components/fields/Select'
 import Textarea from 'orionsoft-parts/lib/components/fields/Textarea'
-import Checkbox from 'orionsoft-parts/lib/components/fields/Checkbox'
 import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
 import withMessage from 'orionsoft-parts/lib/decorators/withMessage'
 import withMutation from 'react-apollo-decorators/lib/withMutation'
 import gql from 'graphql-tag'
-import autobind from 'autobind-decorator'
-import includes from 'lodash/includes'
-import remove from 'lodash/remove'
 
 @withRouter
 @withMessage
@@ -44,48 +41,6 @@ export default class AppRegistrationForm extends React.Component {
     }
   }
 
-  @autobind
-  setUserFields(fieldName, value) {
-    console.log('fieldName:', fieldName, 'value:', value)
-    let currentUserFields = this.state.userFields
-    if (value) {
-      if (!includes(currentUserFields, fieldName)) {
-        currentUserFields.push(fieldName)
-        this.setState({ userFields: currentUserFields })
-      }
-    } else {
-      if (includes(currentUserFields, fieldName)) {
-        remove(currentUserFields, field => field === fieldName)
-        this.setState({ userFields: currentUserFields })
-      }
-    }
-  }
-
-  @autobind
-  renderUserFields() {
-    const userFields = [
-      { label: 'Rut', value: 'identifier' },
-      { label: 'Nombre', value: 'firstName' },
-      { label: 'Apellido', value: 'lastName' },
-      { label: 'Dirección', value: 'address' },
-      { label: 'Teléfono', value: 'phone' },
-      { label: 'Nivel educacional', value: 'educationalLevel' }
-    ]
-
-    return userFields.map(field => {
-      return (
-        <Checkbox
-          label={field.label}
-          key={field.value}
-          onChange={changes => {
-            console.log('changes:', changes)
-            this.setUserFields(field.label, field.value)
-          }}
-        />
-      )
-    })
-  }
-
   render() {
     return (
       <Section
@@ -95,7 +50,7 @@ export default class AppRegistrationForm extends React.Component {
         top
       >
         <Form state={this.state} onChange={changes => this.setState(changes)}>
-          <div className="label">Información de la aplicación:</div>
+          <div className="headerLabel">Información de la aplicación:</div>
           <div className="label">Nombre:</div>
           <Field fieldName="name" type={Text} />
           <div className="label">Descripción:</div>
@@ -103,20 +58,89 @@ export default class AppRegistrationForm extends React.Component {
           <div className="label">URL de redirección:</div>
           <Field fieldName="applicationURL" type={Text} />
           <div className="label">Datos de usuario:</div>
-          {this.renderUserFields()}
-          <div className="label">Email:</div>
-          <Field fieldName="email" type={Text} />
-          <div className="label">Email:</div>
-          <Field fieldName="email" type={Text} />
-          <div className="label">Email:</div>
-          <Field fieldName="email" type={Text} />
-          <div className="label">Email:</div>
-          <Field fieldName="email" type={Text} />
-          <div className="label">Email:</div>
-          <Field fieldName="email" type={Text} />
-          <div className="label">Email:</div>
-          <Field fieldName="email" type={Text} />
-          <Field fieldName="email" type={Text} />
+          <Field
+            fieldName="userFields"
+            type={Select}
+            multi
+            options={[
+              {
+                label: 'Rut',
+                value: 'identifier'
+              },
+              {
+                label: 'Nombre',
+                value: 'firstName'
+              },
+              {
+                label: 'Apellido',
+                value: 'lastName'
+              },
+              {
+                label: 'Dirección',
+                value: 'address'
+              },
+              {
+                label: 'Teléfono',
+                value: 'phone'
+              },
+              {
+                label: 'Nivel Educacional',
+                value: 'educationalLevel'
+              }
+            ]}
+          />
+          <div className="headerLabel">Información del desarrollador:</div>
+          <div className="label">Nombre:</div>
+          <Field fieldName="developerInfo.firstName" type={Text} />
+          <div className="label">Apellido:</div>
+          <Field fieldName="developerInfo.lastName" type={Text} />
+          <div className="headerLabel">Información de contacto:</div>
+          <div className="label">Dirección:</div>
+          <div className="label">Nombre de calle:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.address.streetName"
+            type={Text}
+          />
+          <div className="label">Numeración:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.address.streetNumber"
+            type={Text}
+          />
+          <div className="label">
+            Número de oficina/casa/departamento (opcional):
+          </div>
+          <Field
+            fieldName="developerInfo.contactInformation.address.departmentNumber"
+            type={Text}
+          />
+          <div className="label">Ciudad:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.address.city"
+            type={Text}
+          />
+          <div className="label">Código Postal:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.address.postalCode"
+            type={Text}
+          />
+          <div className="label">Teléfono:</div>
+          <div className="label">Código de área:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.phone.areaCode"
+            type={Text}
+          />
+          <div className="label">Número fijo:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.phone.number"
+            type={Text}
+          />
+          <div className="label">Celular:</div>
+          <Field
+            fieldName="developerInfo.contactInformation.phone.mobilePhone"
+            type={Text}
+          />
+          <div className="label">Pagina Web:</div>
+          <Field fieldName="developerInfo.url" type={Text} />
         </Form>
         <br />
         <Button to="/" style={{ marginRight: 10 }}>
