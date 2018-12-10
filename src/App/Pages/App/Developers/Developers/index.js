2e8444bfb4e1fa35ce21d5da1e90a19765346bf6
Joaquín Gumucio <jjgumucio@gmail.com>
2018-12-10 00:11:47 -0300

    Adds login to registration process

--- src/App/Pages/App/Developers/Developers/index.js
+++ src/App/Pages/App/Developers/Developers/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Section from 'App/components/Section'
 import PropTypes from 'prop-types'
 import withMessage from 'orionsoft-parts/lib/decorators/withMessage'
+import withUserId from 'App/helpers/auth/withUserId'
+import autobind from 'autobind-decorator'
 import Account from './Account'
 import DeveloperInfo from './DeveloperInfo'
+import Button from 'orionsoft-parts/lib/components/Button'
+import { withRouter } from 'react-router-dom'
 
+@withRouter
 @withMessage
+@withUserId
 export default class RegisterDeveloper extends React.Component {
   static propTypes = {
     showMessage: PropTypes.func,
-    createDeveloper: PropTypes.func
+    createDeveloper: PropTypes.func,
+    history: PropTypes.object,
+    userId: PropTypes.string
   }
 
   state = {
@@ -17,9 +25,29 @@ export default class RegisterDeveloper extends React.Component {
     userId: ''
   }
 
+  componentDidMount() {
+    let { userId } = this.props
+    if (userId) {
+      this.setState({ form: 'info', userId })
+    }
+  }
+
+  @autobind
+  redirectToLogin() {
+    let { history } = this.props
+    history.push('/login')
+  }
+
   getSectionMessage() {
     if (this.state.form === 'account') {
-      return 'Crea una cuenta de usuario'
+      return (
+        <div>
+          <p>Crea una cuenta de usuario o inicia sesión:</p>
+          <Button onClick={this.redirectToLogin} primary>
+            Login
+          </Button>
+        </div>
+      )
     } else {
       return 'Ingresa tus datos de usuario'
     }
