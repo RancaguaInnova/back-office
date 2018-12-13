0f54c8f5bef7717d5804109283e634897705405b
Joaquín Gumucio <jjgumucio@gmail.com>
2018-12-13 17:04:18 -0300

    DynamicComponent

--- src/App/Pages/index.js
+++ src/App/Pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import authRouteRegex from './Auth/routeRegex'
-import {withRouter} from 'react-router'
+import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
 import DynamicComponent from 'App/components/DynamicComponent'
 import App from './App'
@@ -13,8 +13,7 @@ export default class Pages extends React.Component {
 
   render() {
     if (authRouteRegex.test(this.props.location.pathname)) {
-      const Component = DynamicComponent(() => import('./Auth'))
-      return <Component />
+      return DynamicComponent(() => import('./Auth'))
     }
     return <App />
   }
