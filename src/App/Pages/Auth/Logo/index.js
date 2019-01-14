ee7d0e6bb8d2f83c87ebd7c35970f832d3f5716a
Joaquín Gumucio <jjgumucio@gmail.com>
2019-01-14 12:27:28 -0300

    Translates forgot

--- src/App/Pages/Auth/Logo/index.js
+++ src/App/Pages/Auth/Logo/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './styles.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default class Logo extends React.Component {
   static propTypes = {}
 
-  render() {
+  render () {
     return (
       <div className={styles.container}>
-        <Link to="/">Corporación Desarrollo e Innovación</Link>
+        <Link to='/'>Rancagua Digital</Link>
       </div>
     )
   }
