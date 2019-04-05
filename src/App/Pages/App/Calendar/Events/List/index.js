6f79656678fd51d991b3cd1cca7fe75adfc4176f
JEPEREZR <jaime@smart.rancagua.cl>
2019-04-05 12:53:55 -0300

    add date in event list

--- src/App/Pages/App/Calendar/Events/List/index.js
+++ src/App/Pages/App/Calendar/Events/List/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PaginatedList from 'App/components/Crud/List'
+import moment from 'moment'
 
 export default class Events extends React.Component {
   getFields() {
@@ -9,6 +10,13 @@ export default class Events extends React.Component {
         title: 'Nombre',
         sort: 'DESC',
         render: (value, doc, index) => <span>{value.name} </span>
+      },
+      {
+        name: 'date.dateStr',
+        title: 'Fecha',
+        render: (value, doc, index) => (
+          <span>{moment(value.date.dateStr).format('DD-MM-YYYY')} </span>
+        )
       },
       {
         name: 'externalUrl',
