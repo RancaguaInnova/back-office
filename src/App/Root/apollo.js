613f786c9a1ae2c4849d265f5bbe7f7f9723ee32
Joaquín Gumucio <jjgumucio@gmail.com>
2019-06-06 13:48:49 -0400

    format fix

--- src/App/Root/apollo.js
+++ src/App/Root/apollo.js
@@ -1,13 +1,13 @@
-import {createClient} from '@orion-js/graphql-client'
+import { createClient } from '@orion-js/graphql-client'
 import url from './url'
 
 export default createClient({
   endpointURL: url,
   useSubscriptions: false,
-  saveSession(session) {
+  saveSession (session) {
     localStorage.setItem('session', JSON.stringify(session, null, 2))
   },
-  getSession(session) {
+  getSession (session) {
     try {
       return JSON.parse(localStorage.getItem('session'))
     } catch (e) {
