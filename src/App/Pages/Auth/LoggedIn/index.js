f7fca5fa87b5eb7dcb954e5bdf2a19200fd4efdc
JEPEREZR <jaime@smart.rancagua.cl>
2019-06-10 15:52:12 -0400

    Update index.js
    se agrega try catch y await en logout

--- src/App/Pages/Auth/LoggedIn/index.js
+++ src/App/Pages/Auth/LoggedIn/index.js
@@ -20,10 +20,12 @@ export default class Logout extends React.Component {
     logout: PropTypes.func
   }
   @autobind
-  async logout() {
-    await this.props.logout({ sessionId: this.props.userId })
-    LogoutHelp()
-    this.props.history.push('/login')
+  async logoutSession() {
+    try {
+      await this.props.logout({ sessionId: this.props.userId })
+      await LogoutHelp()
+      this.props.history.push('/login')
+    } catch (error) {}
   }
 
   render() {
@@ -31,7 +33,7 @@ export default class Logout extends React.Component {
       <div className={styles.container}>
         <p>Ya tienes tu sesión iniciada, quieres cerrar tu sesión?</p>
         <Button onClick={() => this.props.history.push('/')}>Home</Button>
-        <Button onClick={this.logout} danger>
+        <Button onClick={this.logoutSession} danger>
           Cerrar Sesión
         </Button>
       </div>
